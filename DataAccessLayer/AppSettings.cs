22cd9a485b6d54265d770d4880876c564c5b5013
iamsuryachauhan <31027430+iamsuryachauhan@users.noreply.github.com>
2023-03-17 20:19:28 +0530

    DataAccessLayer for API

---
+++ DataAccessLayer/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class AppSettings : IAppSetting
+    {
+        private readonly IConfiguration _configuration;
+        public AppSettings(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string ConnectionString => _configuration["ConnectionStrings:LabourDBConn"];
+    }
+}
---
+++ DataAccessLayer/Models/M_LabourInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class M_LabourInfo
+    {
+        public string LabourCode { get; set; }
+        public string LabourName { get; set; }
+        public string DOB { get; set; }
+        public string Location { get; set; }
+        public string PhoneNo { get; set; }
+        public string Image { get; set; }
+        public string Gender { get; set; }
+    }
+}
